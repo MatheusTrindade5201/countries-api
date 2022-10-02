682e61e47537419af9f25c509196642b4d69185f
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-02 18:23:56 -0300

    Alteração na estrtura HTML do cabeçalho - Semântica

--- src/components/cabecalho/index.js
+++ src/components/cabecalho/index.js
@@ -2,10 +2,10 @@ import './cabecalho.css'
 
 const Cabecalho = () => {
     return (
-        <nav className="cabecalho">
+        <header className="cabecalho">
             <h2 className="cabecalho__titulo">Where in the world?</h2> 
             <div className="cabecalho__tema-interruptor"><span className="cabecalho__tema-icone"></span>Dark Mode</div>
-        </nav>
+        </header>
     )
 }
 
