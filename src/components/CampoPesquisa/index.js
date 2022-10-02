41f8f27e1d9cb0d965120873c7077220f707040f
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-02 19:03:24 -0300

    Componente do campo de pesquisa

---
+++ src/components/CampoPesquisa/index.js
@@ -0,0 +1,9 @@
+import './CampoPesquisa.css'
+
+const CampoPesquisa = () => {
+    return (
+        <input placeholder='Search for a country' className='campoPesquisa' />
+    )
+}
+
+export default CampoPesquisa
