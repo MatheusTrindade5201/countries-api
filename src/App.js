6793b9df5ad613ecc2e53c93aed209cb0d83e8f0
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-05 22:07:13 -0300

    Correção da estrutura de lógica do consumo de APi para os padrões do React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
+import { useEffect, useState } from "react";
 import Cabecalho from "./components/Cabecalho";
 import Filtro from "./components/Filtro";
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <Cabecalho/>
-      <Filtro />
-    </div>
-  );
+
+  const [paises, setPaises] = useState(false)
+
+  const ConsomeApi = async () => {
+    try {
+        const data = await fetch('https://restcountries.com/v3.1/all');
+        const json = await data.json()
+        setPaises(json)
+        console.log(json);     
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+  useEffect(() => {
+   ConsomeApi();
+  }, [])
+
+  if(paises == false ){
+    return (
+      <div className="App">
+        <Cabecalho/>
+        <Filtro />
+        <p>Carregando...</p>
+      </div>
+    );
+  }else{
+    return (
+      <div className="App">
+        <Cabecalho/>
+        <Filtro />
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
