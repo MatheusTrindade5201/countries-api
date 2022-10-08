356c3d265fd530ec02879a000897969046220c14
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-08 17:35:29 -0300

    Limpando comnetários

--- src/App.js
+++ src/App.js
@@ -5,26 +5,6 @@ import Rotas from "./Routes/routes";
 
 function App() {
 
-//   const [regiao, setRegiao] = useState('') 
-
-//   const [paises, setPaises] = useState(false)
-//   const [busca, setBusca] = useState('')
-
-//   const ConsomeApi = async () => {
-//     try {
-//         const data = await fetch('https://restcountries.com/v3.1/all');
-//         const json = await data.json()
-//         setPaises(json)
-//         console.log(json);     
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
-
-//   useEffect(() => {
-//    ConsomeApi();
-//   }, [])
-
   return (
     <Rotas />
   )
