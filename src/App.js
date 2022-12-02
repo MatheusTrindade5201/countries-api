14d9df72be1f84008a5389ec091514f942119b31
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-12-02 20:48:48 -0300

    feat: criando um state para o tema

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
+import { MyContextProvider } from "./context/myContext";
 import Rotas from "./Routes/routes";
-
-
-
 
 function App() {
 
+  const [theme, setTheme] = useState('')
+
   return (
+    <MyContextProvider value={{theme, setTheme}}>
     <Rotas />
+    </MyContextProvider>
   )
 }
 
